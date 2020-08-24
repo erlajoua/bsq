78d81c0f168e16f4d06a115b8f266a59d2792dcd
Mahaut LATINIS <malatini@e3r6p18.42.fr>
2020-08-24 14:08:40 +0200

    creation de la map - structure map

--- mahaut.h
+++ mahaut.h
@@ -8,6 +8,12 @@ typedef	struct	s_read
 	char		**strs;
 }				t_read;
 
+typedef	struct	s_map
+{
+	int			i;
+	int			y;
+}				t_map;
+
 int				**from_map_to_strs(int argc, char **argv);
 char			**ft_putstrs(char **strs, char *str, int i);
 char			**ft_strs_alloc(char *map);
@@ -19,5 +25,8 @@ char			**ft_split(char *str, char *charset);
 char			*ft_strdup(char *str, char *charset);
 int				count_words(char *str, char *charset);
 int				part_of_charset(char c, char *charset);
+char			**from_chars_to_01(char **strs, char empty, char filled);
+t_map			*from_01_to_map(char **strs);
+int				nb_of_strs(char **strs);
 
 #endif
